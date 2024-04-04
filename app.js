46f6bda5b1d11d35c1971a2306b6a06c235405fe
Asma <asma.khokharr@gmail.com>
2024-04-04 19:37:28 +0500

    added
    added

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+/**
+ * PRELOAD
+ * 
+ * Loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
